8a7f43ca4e30b99a28ae44ace2be12b598e371a3
Gurveer <gurveer7@hotmail.com>
2023-01-25 04:16:30 -0500

    refreshes new quotes

--- main.js
+++ main.js
@@ -7,19 +7,28 @@ const options = {
 	}
 };
 
-fetch('https://quotes15.p.rapidapi.com/quotes/random/', options)
-	.then(response => response.json())
-	.then(response => {
-        console.log(response) //For Testing Purposes
-
-        const card = document.querySelector('.card');
-        card.addEventListener('mouseover', function() {
-        card.addEventListener('click', function() {
-            window.location.href = response.url;
-        });
-    });
+window.onload = function() {
+    fetch('https://quotes15.p.rapidapi.com/quotes/random/', options)
+      .then(response => response.json())
+      .then(response => {
         document.getElementById('quote').innerHTML = response.content
         document.getElementById('author').innerHTML = "- " + response.originator.name
-    })
-	.catch(err => console.error(err));
+      })
+      .catch(err => console.error(err));
+  
+    let intervalTimes = setInterval(() => {
+      fetch('https://quotes15.p.rapidapi.com/quotes/random/', options)
+      .then(response => response.json())
+      .then(response => {
+        document.getElementById('quote').innerHTML = response.content
+        document.getElementById('author').innerHTML = "- " + response.originator.name
+      })
+      .catch(err => console.error(err));
+    }, 10000);
+
+    // setTimeout(() => {
+    //     clearInterval(intervalTimes);
+    //   }, 10000);
+  };
+  
 
