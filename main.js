25d6fdc7e0dce23c730f9060c5917abf535fb8fc
Gurveer <gurveer7@hotmail.com>
2023-01-25 04:00:07 -0500

    styling and url pointing

--- main.js
+++ main.js
@@ -10,8 +10,16 @@ const options = {
 fetch('https://quotes15.p.rapidapi.com/quotes/random/', options)
 	.then(response => response.json())
 	.then(response => {
-        console.log(response)
+        console.log(response) //For Testing Purposes
+
+        const card = document.querySelector('.card');
+        card.addEventListener('mouseover', function() {
+        card.addEventListener('click', function() {
+            window.location.href = response.url;
+        });
+    });
         document.getElementById('quote').innerHTML = response.content
         document.getElementById('author').innerHTML = "- " + response.originator.name
     })
 	.catch(err => console.error(err));
+
